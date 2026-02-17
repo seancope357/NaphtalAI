060d46b9ec7ca27d356eec918d6c75eacd5d1f55
seancope357 <seancope357@gmail.com>
2026-02-17 12:25:55 -0600

    fix: Add 'use client' to Hero component

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { useState, useEffect } from "react";
